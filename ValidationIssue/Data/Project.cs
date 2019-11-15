e4c90b8388de67661795d88cbfd8f78734f67b32
Katie Prodoehl <katie.prodoehl@legrand.us>
2019-11-15 10:46:30 -0600

    Add project files.

---
+++ ValidationIssue/Data/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ValidationIssue.Data.Validation;
+
+namespace ValidationIssue.Data
+{
+    public class Project
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Request By Date")]
+        [CurrentOrFutureDateValidation]
+        public DateTime? RequestedByDate { set; get; }
+    }
+}
---
+++ ValidationIssue/Data/Validation/CurrentorFutureDateValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ValidationIssue.Data.Validation
+{
+    class CurrentOrFutureDateValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            DateTime dt = (DateTime)value;
+            if (dt >= DateTime.Today)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage ?? "Date selected must be greater or equal to today's date.");
+        }
+    }
+}
